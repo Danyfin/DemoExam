085d71957e79bf83fcccd81cc80307d9484c5831
Danyfin <gdanil871@gmail.com>
2025-12-25 15:22:55 +0500

    ready

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Status;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,15 +15,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        Status::created(['title' => 'новая']);
+        Status::created(['title' => 'в процессе']);
+        Status::created(['title' => 'завершена']);
+        Status::created(['title' => 'отменена']);
 
         User::firstOrCreate(
-            ['email' => 'test@example.com'],
             [
-                'name' => 'Test User',
-                'password' => 'password',
-                'email_verified_at' => now(),
-            ]
-        );
+                'firstname' => 'Admin',
+                'middlename' => 'Admin',
+                'lastname' => 'Admin',
+                'email' => 'admin@example.com',
+                'login' => 'administrator',
+                'password' => Hash::make('administrator'),
+                'tel' => '+79999999999',
+                'role' => 'admin',
+
+            ]);
     }
 }
